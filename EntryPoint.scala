a4e93803d48e8a6882d5e6e9842c1e85672995cc
tomm <gdunghi@gmail.com>
2022-07-30 22:49:08 +0700

    homework

--- EntryPoint.scala
+++ EntryPoint.scala
@@ -1,13 +1,51 @@
 object EntryPoint extends App {
 
   import scalaj.http._
-  val response: HttpResponse[String] =
-    Http("http://feeds.bbci.co.uk/news/rss.xml").asString
-
+  import scala.io.Source
+  import java.io.FileWriter
   import scala.xml.XML
-  val elements = XML.loadString(response.body)
-  val titles = elements.\\("item").\("title")
-  for (title <- titles) {
-    println(title.text)
+  import scala.xml.NodeSeq
+
+  val urls = Source.fromFile("source.txt").getLines().toList
+  val filterUkraine = filter("Ukraine") _
+
+  (fetchFeeds _ andThen filterUkraine andThen saveToFile) (urls)
+
+  def getContent(url: String): Seq[String] = {
+    val res: HttpResponse[String] = Http(url).asString
+    val elements = XML.loadString(res.body)
+    val titles = elements \\ "item" \ "title"
+    val result = titles.map(title => title.text)
+    result
   }
+
+  def fetchFeeds(urls: Seq[String]): Seq[String] = {
+    val contents = urls
+      .map(url => getContent(url)).toList
+      .reduce((a, b) => {
+        a ++ b
+      })
+    contents
+
+  }
+
+  def filter(keyword: String)(contents: Seq[String]): Seq[String] = {
+    val result = contents
+      .filter(content => content.toLowerCase() contains keyword.toLowerCase())
+    result
+  }
+
+  def saveToFile(contents: Seq[String]) = {
+    import java.text.SimpleDateFormat
+    import java.util.Calendar
+    val format = new SimpleDateFormat("d-M-y-hh-mm")
+    val fileName = "ukraine-" + format.format(Calendar.getInstance().getTime()) + ".txt"
+    val fw = new FileWriter(fileName, true);
+    for (title <- contents) {
+      fw.write(title + "\n")
+    }
+    fw.close()
+  }
+
+
 }
